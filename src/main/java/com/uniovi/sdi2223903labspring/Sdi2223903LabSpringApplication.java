3191735faff8d776546c877e9d8a3bf821364bd9
UO271572 <uo271572@uniovi.es>
2023-02-06 11:25:22 +0100

    SDI-2223-903-2.1-Create Spring Proyect

---
+++ src/main/java/com/uniovi/sdi2223903labspring/Sdi2223903LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223903labspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223903LabSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223903LabSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2223903labspring/Sdi2223903LabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223903labspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223903LabSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
