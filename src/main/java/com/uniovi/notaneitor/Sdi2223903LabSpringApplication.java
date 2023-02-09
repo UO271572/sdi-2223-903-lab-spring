8db55c4ffa18a4528da3a46f52cc1a1cd75aac23
UO271572 <uo271572@uniovi.es>
2023-02-09 19:07:46 +0100

    Fixed

--- src/main/java/com/uniovi/notaneitor/Sdi2223903LabSpringApplication.java
+++ src/main/java/com/uniovi/notaneitor/Sdi2223903LabSpringApplication.java
@@ -1,4 +1,4 @@
-package com.uniovi.notaneitor.sdi2223903labspring;
+package com.uniovi.notaneitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/uniovi/notaneitor/ServletInitializer.java
+++ src/main/java/com/uniovi/notaneitor/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.uniovi.notaneitor.sdi2223903labspring;
+package com.uniovi.notaneitor;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
