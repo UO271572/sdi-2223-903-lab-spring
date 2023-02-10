20b4e8b54130f64fccf43deab5e20a65ff14a06f
UO271572 <uo271572@uniovi.es>
2023-02-10 11:30:11 +0100

    “SDI-2021-903-2.2-Trabajando con Controladores

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -1,22 +1,28 @@
 package com.uniovi.notaneitor.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.uniovi.notaneitor.entities.Mark;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MarksController {
 
     @RequestMapping("/mark/list")
-    public String getList() {
+    public String getList()
+    {
         return "Getting List";
     }
-    @RequestMapping("/mark/add")
-    public String setMark() {
-        return "Adding Mark";
+
+    @RequestMapping(value="/mark/add", method = RequestMethod.POST)
+    public String setMark(@ModelAttribute Mark mark)
+    {
+        return "added: " + mark.getDescription()
+                + " with score : " + mark.getScore()
+                + " id: " + mark.getId();
     }
-    @RequestMapping("/mark/details")
-    public String getDetail(@RequestParam Long id) {
+
+    @RequestMapping("/mark/details/{id}")
+    public String getDetail(@PathVariable Long id)
+    {
         return "Getting Details =>" + id;
     }
 
