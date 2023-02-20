436b5384ec30774c8627ee1e461a6e79993304f4
UO271572 <uo271572@uniovi.es>
2023-02-20 09:59:25 +0100

    SDI-2223-903-sesión3-Ejercicio-complementario1

--- src/main/java/com/uniovi/notaneitor/services/UsersService.java
+++ src/main/java/com/uniovi/notaneitor/services/UsersService.java
@@ -18,6 +18,8 @@ public class UsersService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+
+
     @PostConstruct
     public void init() {
     }
@@ -30,10 +32,16 @@ public class UsersService {
     public User getUser(Long id) {
         return usersRepository.findById(id).get();
     }
+
     public void addUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
     }
+
+    public void editUser(User user) {
+        usersRepository.save(user);
+    }
+
     public User getUserByDni(String dni) {
         return usersRepository.findByDni(dni);
     }
